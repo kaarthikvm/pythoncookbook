601a5cbb854f82f5909ee9fa9f8fcdfe35a1a2c4
kaarthik.vm@gmail.com <kaarthik.vm@gmail.com>
2018-08-11 15:40:22 -0700

    pubsub test using callback

--- redis-python/myredis.py
+++ redis-python/myredis.py
@@ -1,30 +1,72 @@
 #!/usr/bin/python
+# API reference : https://github.com/andymccurdy/redis-py
 import redis
 
 
 redis_host = "localhost"
 redis_port = 6379
 redis_password = ""
-
+r = None;
+pubsubptr = None;
 
 def sample_redis():
     """ Sample code to access redis server """
     
     try:
     
-        # The decode_repsonses flag here directs the client to convert the responses from Redis into Python strings
-        # using the default encoding utf-8.  This is client specific.
+        # The decode_repsonses flag used to give response as python strings (default encoding utf-8)
+        global r;
+        global pubsubptr;  
+
         r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
-        
-        r.set("user1", "granted access")
+   
+        # handler to hold pubsub method
+        pubsubptr = r.pubsub();         
+        # pubsubptr = r.pubsub(ignore_subscribe_messages=True);         
+ 
+        r.set("user1", "apple")
 
         # step 5: Retrieve the hello message from Redis
         msg = r.get("user1")
         print msg        
-    
+ 
+ 
     except Exception as e:
         print e
 
 
+
+def multi_exec():
+    """ MULTI and EXEC in Redis can be tried using pipeline command """
+    pipe = r.pipeline();
+    pipe.set("user2","mango");
+    pipe.get("user1");
+    pipe.execute()
+
+def my_cb(message):
+    """  callback to retrieve the message from channel """
+    print "+++++++++++++++++++++++++++++++++++++++++++++++++"
+    print "        " + message['data'];
+    print "+++++++++++++++++++++++++++++++++++++++++++++++++"
+
+
+def subscribe_cbmethod():
+    """ subscribe using callback method 
+        channel name is my-channel
+        callback name is my_cb """
+
+    pubsubptr.subscribe(**{'my-channel': my_cb}); 
+    print pubsubptr.get_message(); # each time get_message is used to get subscribe message
+    # to ignore this use
+    # r.pubsub(ignore_subscribe_messages=True)
+    r.publish('my-channel', 'Global Warming - world has to rise to stop')
+    pubsubptr.get_message();
+
 if __name__ == '__main__':
     sample_redis()
+    multi_exec();
+    subscribe_cbmethod();
+    #pubsubptr.subscribe('ch1');
+    #print pubsubptr.get_message();
+    #r.publish('ch1','some data');
+    #print pubsubptr.get_message();
