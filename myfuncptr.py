2c3df8c78debf594b122c9d1ad95c414795a6330
kaarthik.vm@gmail.com <kaarthik.vm@gmail.com>
2018-09-14 15:15:07 -0700

    updated decorator

--- myfuncptr.py
+++ myfuncptr.py
@@ -27,3 +27,32 @@ def hello_wrapper_nested_func(func):
 func_obj = hello_wrapper_nested_func(hello);
 print "\n \n";
 func_obj(); # this is equivalent to decorator
+
+
+
+
+def decorator_wrapper( func ):
+    print "decorator function";
+    def inner_func( arg ):
+        func(arg);
+    return inner_func;
+
+
+@decorator_wrapper
+def display( arg ):
+    print "display " + arg ;
+
+# func_obj = decorator_wrapper(display);
+# func_obj("input 2");
+# OR OR OR OR
+# display = decorator_wrapper(display);
+# display("input 2");
+
+# above lines can be simulated using decorator
+# if decorator is defined then below is commented as decorator performs same thing
+#display = decorator_wrapper(display);
+display("input 2");
+
+
+
+
