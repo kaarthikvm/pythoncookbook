ca57b07e10bed3a416b73cfb535c3870b3569998
kaarthik.vm@gmail.com <kaarthik.vm@gmail.com>
2019-01-20 04:20:19 -0800

    setup file

--- wpengine/setup.py
+++ wpengine/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name = "bbtask",
+    name = "task",
     version = "0.0.1",
-    author = "Kaarthik",
-    author_email = "kaarthik.vm@gmail.com",
-    description = ("Python project framerwork for evaluation"),
+    author = "",
+    author_email = "",
+    description = ("Python project for wpengine  evaluation"),
     license = "BSD",
-    packages=['bbtask', 'tests'],
+    packages=['task', 'tests'],
 )
