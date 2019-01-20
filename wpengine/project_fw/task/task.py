37bd0cfcb1f1e9618a09d27b0c4bb4170b5a7dc9
kaarthik.vm@gmail.com <kaarthik.vm@gmail.com>
2019-01-20 01:32:36 -0800

    updated code

--- wpengine/project_fw/task/task.py
+++ wpengine/project_fw/task/task.py
@@ -3,6 +3,10 @@ import sys
 import csv
 import constant as c
 import requests
+from collections import OrderedDict
+
+wr_hdlr= None;
+csv_wr= None;
 
 def debug_print ( args ):
     #print "#################################\n";
@@ -10,38 +14,57 @@ def debug_print ( args ):
     #print "\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n";
 
 def get_account_status ( account_id ):
-    debug_print(c.URI + account_id);
+    debug_print("URL ==> " + c.URI + account_id);
     resp = requests.get(c.URI + account_id);
     resp.raise_for_status(); # raises exception for other than 200 status code
-    debug_print("API response ==> " + str(resp.json())); 
+    debug_print("URL API response ==> " + str(resp.json())); 
+    return resp.json();
 
-def read_csv ( fileName ):
-    with open( fileName, 'rb') as rd_hdlr:
-        #csv_rdr = csv.reader(rd_hdlr, delimiter=',');
-        csv_rdr = csv.DictReader(rd_hdlr);#, delimiter=',');
-        count = 0;
-        for row in csv_rdr:
-            #debug_print(row);
-            #if count == 0:
-                # column header
-            #    count += 1;
-            #    continue; 
-            get_account_status(row['Account ID']);
+def process_csv ( ifileName, ofileName ):
+    ofnames = OrderedDict([('Account ID', None),
+                           ('First Name', None),
+                           ('Created On', None),
+                           ('Status', None),
+                           ('Status Set', None)]);
+ 
+    with open( ifileName, 'rb') as rd_hdlr:
+        csv_rdr = csv.DictReader(rd_hdlr);
+        # init csv file for writing
+        with open(ofileName, 'wb') as wr_hdlr:
+            csv_wr = csv.DictWriter(wr_hdlr, delimiter=',', fieldnames=ofnames)
+            csv_wr.writeheader();
+            for data in csv_rdr:
+                debug_print("\nInput file content ==> " + str(data));
+                resp = get_account_status(data['Account ID']);
+                d= {'Account ID': resp['account_id'],
+                    'First Name': data['First Name'],
+                    'Created On': str(resp['created_on']),
+                    'Status': str(resp['status']),
+                    'Status Set': 'On'};# if resp['status'] != None};
+                debug_print("Output to csv file ==>  " + str(d));
+                csv_wr.writerow(d); 
 
-
-def write_csv ( fileName ):
-    pass
+def csv_write_init ( ofileName):
+    ofnames = OrderedDict([('Account ID', None),
+                           ('First Name', None),
+                           ('Created On', None),
+                           ('Status', None),
+                           ('Status Set', None)]);
+    global wr_hdlr;
+    wr_hdlr = open( ofileName, 'wb');
+    global csv_wr;
+    csv_wr = csv.DictWriter(wr_hdlr, delimiter=',', fieldnames=ofnames)
+    csv_wr.writeheader();
 
 def main (input_file, output_file):
     try:
-        read_csv(input_file);
-        write_csv(output_file); 
+        process_csv(input_file, output_file); 
     except requests.exceptions.RequestException as e:
         debug_print("REST API exception \n %s" % (e));
         sys.exit(1);  
     except IOError as e:
         debug_print("I/O error({0}): {1}".format(e.errno, e.strerror));
-
+    
 if __name__ == "__main__":
     debug_print("Length of arguments  %d" % (len(sys.argv)));
     debug_print("List of arguments    %s" % (str(sys.argv)));
